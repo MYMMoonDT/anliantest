73c7018d839cd6218b0124e253f9e67cb4d44ef2
MYMMoonDT <bixiaodong1990@163.com>
2013-11-21 22:03:54 +0800

    Add home and login page Create some folders for web resources

--- anliantest-web/src/main/java/org/tongji/anliantest/controller/UserController.java
+++ anliantest-web/src/main/java/org/tongji/anliantest/controller/UserController.java
@@ -2,11 +2,13 @@ package org.tongji.anliantest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("user")
 public class UserController extends AbstractController {
-	@RequestMapping("login")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(){
-		return "home";
+		return "ui/page/home";
 	}
 }
